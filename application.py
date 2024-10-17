abbbf48554935cf51af966aeba81fbd75dca596d
Oscar Namicano <onamicanosail01@gmail.com>
2024-10-17 02:10:22 +0200

    Changes in the index to adapt for multiple establishments

--- application.py
+++ application.py
@@ -1070,10 +1070,10 @@ def subscribe_employer(establishment_alias):
 
 
 if __name__ == "__main__":
-
+    '''
     port = int(os.getenv('PORT'), '5000')
     #We are getting the port where our server is running, else we use the 5000
     app.run(host='0.0.0.0', port=port)
-
+'''
     
-    #app.run(debug=True)
+    app.run(debug=True)
